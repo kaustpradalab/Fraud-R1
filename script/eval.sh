e39f4bc739e90691b4270b9b1f811933c57f9715
Shenzhe Zhu <zhushenzhe0531@gmail.com>
2025-02-07 01:17:34 -0500

    update

---
+++ script/eval.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 设置输入文件夹
+EVAL_FOLDER="./results/baseline"
+OUTPUT_FILE="./results/baseline/eval/asr.xlsx"
+
+python main.py --eval_input_folder "$EVAL_FOLDER"  --eval_output_file "$OUTPUT_FILE" --mode eval
